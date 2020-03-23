869536fe11e5789819044cab6e485784b899b31b
Joe Guo <joe.guo@canonical.com>
2020-03-23 18:14:38 +1300

    re-org code
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- remember.py
+++ remember.py
@@ -1,40 +1,50 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 from os import getenv
 from os import makedirs
 from os.path import join
 from datetime import datetime, timedelta
 
-import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 
 APP_NAME = 'remember'
 HOME = getenv('HOME')
 CONFIG_HOME = '{}/.config/{}'.format(HOME, APP_NAME)
-makedirs(CONFIG_HOME, exist_ok=True)
-
 DB_PATH = join(CONFIG_HOME, '{}.db'.format(APP_NAME))
 DB_URL = URL(drivername='sqlite', database=DB_PATH)
-print(DB_URL)
 
+makedirs(CONFIG_HOME, exist_ok=True)
 
-ENVVAR_PRIFIX = APP_NAME.upper()
-def _getenv(name, default=None):
-    return getenv('{}_{}'.format(ENVVAR_PRIFIX, name), default=default)
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description="Remember Words")
 
+parser.add_argument("-a", "--add", action="store_true", help="Add new word")
+parser.add_argument("--keyword", help="word keyword")
+parser.add_argument("--content", help="word content")
+parser.add_argument("-p", "--pop", action="store_true", help="Pop a word")
+parser.add_argument("-v", "--verbose", action="store_true", help="Be verbose")
 
-print(sqlalchemy.__version__)
+args = parser.parse_args()
 
-from sqlalchemy.ext.declarative import declarative_base
+VERBOSE = args.verbose
+
+if VERBOSE:
+    def verbose(*args, **kwargs):
+        print(*args, file=sys.stderr, **kwargs)
+else:
+    def verbose(*args, **kwargs):
+        pass
+
 
 Base = declarative_base()
-engine = create_engine(DB_URL, echo=True)
-
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship, sessionmaker
-
+engine = create_engine(DB_URL, echo=VERBOSE)
+verbose(DB_URL)
 
 class Word(Base):
 
@@ -63,30 +73,6 @@ class Memo(Base):
 #  engine.connect()
 Base.metadata.create_all(bind=engine)
 
-
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    description="Remember Words",
-)
-
-parser.add_argument(
-    "-a", "--add", action="store_true",
-    help="Add new word")
-
-parser.add_argument(
-    "--keyword",
-    help="word keyword")
-
-parser.add_argument(
-    "--content",
-    help="word content")
-
-parser.add_argument(
-    "-p", "--pop", action="store_true",
-    help="Pop a word from memo list")
-
-args = parser.parse_args()
-
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
@@ -109,10 +95,10 @@ if args.add:
 if args.pop:
     memo = session.query(Memo).order_by(Memo.scheduled_at).first()
     if memo:
-        print(memo.id, memo.scheduled_at)
+        verbose(memo.id, memo.scheduled_at)
         word_id = memo.word_id
         word = session.query(Word).filter(Word.id==word_id).first()
-        print(word.keyword, word.content)
+        verbose(word.keyword, word.content)
         session.query(Memo).filter(Memo.id==memo.id).delete()
         session.commit()
 
